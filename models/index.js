0ff9218ae8faf24c59e53e4935b2b73ce7eb7643
DaveM <dave.miranda@gmail.com>
2020-06-01 22:24:51 -0400

    first week answer

--- models/index.js
+++ models/index.js
@@ -1,17 +1,17 @@
-const Sequelize = require('sequelize')
-const allConfigs = require('../configs/sequelize')
+import Sequelize from 'sequelize'
+import allConfigs from '../configs/sequelize'
 
-const AuthorsModel = require('./authors')
-const GenresModel = require('./genres')
-const NovelsModel = require('./novels')
-const NovelsGenresModel = require('./novelsGenres')
+import AuthorsModel from './authors'
+import GenresModel from './genres'
+import NovelsModel from './novels'
+import NovelsGenresModel from './novelsGenres'
 
-const environment = process.env.NODE_ENV ? process.env.NODE_ENV : 'development'
-const {
+export const environment = process.env.NODE_ENV ? process.env.NODE_ENV : 'development'
+export const {
   username, password, database, host, dialect,
 } = allConfigs[environment]
 
-const connection = new Sequelize(database, username, password, { host, dialect })
+export const connection = new Sequelize(database, username, password, { host, dialect })
 
 const Authors = AuthorsModel(connection, Sequelize)
 const Genres = GenresModel(connection, Sequelize)
---
+++ server.js
@@ -0,0 +1,18 @@
+import express from 'express'
+import { getAllNovels, getNovelsByTitle } from './controllers/novels'
+import path from 'path'
+
+
+const app = express()
+
+app.use(express.static('public'))
+
+app.get('/api/novels', getAllNovels)
+
+app.get('/api/novels/:id', getNovelsByTitle)
+
+app.all('*', (request, response) => response.sendFile(path.resolve(__dirname, 'public', 'index.html')))
+
+app.listen(1337, () => {
+  console.log('Listening on port 1337...') // eslint-disable-line no-console
+})
